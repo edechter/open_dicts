8f1668d8fdf1c14f1549c1b9ac18779731332b5e
Eyal Dechter <eyaldechter@gmail.com>
2016-11-07 12:27:21 -0500

    Add pack.pl file

---
+++ pack.pl
@@ -0,0 +1,8 @@
+name(open_dicts).
+title('Open dicts for SWI Prolog').
+version('0.0.1').
+author('Eyal Dechter', 'eyaldechter@gmail.com').
+maintainer('Eyal Dechter', 'eyaldechter@gmail.com').
+packager('Eyal Dechter', 'eyaldechter@gmail.com').
+download('https://github.com/edechter/open_dicts/releases/*.zip').
+requires(function_expansion).
